831aa68bf5443e04935d281d3879d3d213e278e6
Noah Kalkstein <nkalkstein@gmail.com>
2018-01-04 15:31:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 function dwarfRollCall(dwarves) {
+  var dwarfArray = []
+  for (var i = 0; i < dwarves.length; i++) {
+    dwarfArray.push(i+1 + ". " + dwarves[i] + " ")
+  }
+  return dwarfArray.join('')
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var planeteerCaps = []
+  for (var i = 0; i < planeteerCalls.length; i++) {
+    planeteerCaps.push(planeteerCalls[i].toUpperCase + "!")
+  }
+  return planeteerCaps
 }
 
 function longPlaneteerCalls(words) {
+  var i = 0
+  while (i < words.length) {
+    if (words[i].length > 4) {
+      return true
+}
+    i++
+  }
+  return false
 }
 
 function findTheCheese (foods) {
+  var i = 0
+  do {
+    if (foods[i] == 'cheddar' || foods[i] == 'gouda' || foods[i] == 'camembert') {
+      return foods[i];
+}
+      i++;
+  }
+  while (i < foods.length);
+  return "no cheese!"
 }
